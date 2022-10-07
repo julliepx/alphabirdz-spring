ce7862e50761b40fcaafd8462c4feffaee0a34d7
Jullie Paixão <jlpaixaof@gmail.com>
2022-10-07 13:04:45 -0300

    Updated UserClasses

--- AlphaBirdz/src/main/java/com/alphabirdz/AlphabirdzApplication.java
+++ AlphaBirdz/src/main/java/com/alphabirdz/AlphabirdzApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.alphabirdz.model.Users;
-import com.alphabirdz.repository.UsersRepository;
+import com.alphabirdz.model.User;
+import com.alphabirdz.repository.UserRepository;
 
 
 
@@ -23,22 +23,22 @@ public class AlphabirdzApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(UsersRepository repository) {
+	public CommandLineRunner demo(UserRepository repository) {
 		return (args) -> {
 			// Save a few Addresses
-			repository.save(new Users("https://cdn.discordapp.com/attachments/1019435827157287073/1027814174501371944/download20221001021201.png", "Lolla", "lollapedrita3@gmail.com", "123456"));
-			repository.save(new Users("https://cdn.discordapp.com/attachments/1019435827157287073/1027813731805171722/Prancheta_4.png", "Celso", "celsothedog@hotmail.com", "celsinho123"));
+			repository.save(new User("https://cdn.discordapp.com/attachments/1019435827157287073/1027814174501371944/download20221001021201.png", "lolla", "lollapedrita3@gmail.com", "123456"));
+			repository.save(new User("https://cdn.discordapp.com/attachments/1019435827157287073/1027813731805171722/Prancheta_4.png", "celso", "celsothedog@hotmail.com", "celsinho123"));
 
 			
 
 			log.info("Users found with findAll():");
 			log.info("-------------------------------");
-			for (Users user : repository.findAll()) {
+			for (User user : repository.findAll()) {
 				log.info(user.toString());
 			}
 			log.info("");
 
-			Users user = repository.findById(1L);
+			User user = repository.findById(1L);
 			log.info("User found with findById(1L):");
 			log.info("--------------------------------");
 			log.info(user.toString());
@@ -47,7 +47,7 @@ public class AlphabirdzApplication {
 			// Fetch Addresses by CEP
 			log.info("User found with findByEmail('01004000'):");
 			log.info("--------------------------------------------");
-			Users email = repository.findByEmail("celsothedog@hotmail.com");
+			User email = repository.findByEmail("celsothedog@hotmail.com");
 			// for (Address a : repository.findByCep("01004000")) {
 			// log.info(a.toString());}
 			log.info(email.toString());
--- AlphaBirdz/src/main/java/com/alphabirdz/model/User.java
+++ AlphaBirdz/src/main/java/com/alphabirdz/model/User.java
@@ -1,12 +1,14 @@
 package com.alphabirdz.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Users {
+@Table(name = "ornithologist")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,14 +19,14 @@ public class Users {
     private String email;
     private String password;
 
-    public Users(String profilePhoto, String username, String email, String password) {
+    public User(String profilePhoto, String username, String email, String password) {
         this.profilePhoto = profilePhoto;
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    protected Users() {
+    protected User() {
 
     }
 
