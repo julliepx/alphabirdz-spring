8b80f8b65c0575f3a896a4285b51f87f59dcd793
Jullie Paixão <jlpaixaof@gmail.com>
2022-10-14 10:29:23 -0300

    Ajustes Finais, SEXTOU!

--- server/src/test/java/com/alphabirdztest/BirdRestControllerTest.java
+++ server/src/test/java/com/alphabirdztest/BirdRestControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.bird;
+package com.alphabirdztest;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
