6762ef8e610928f52e2e945f924e9043b8c26542
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-10-17 11:27:41 -0300

    Feat - adicionados novas passaros

--- server/src/main/resources/database/populateDatabase.sql
+++ server/src/main/resources/database/populateDatabase.sql
@@ -1,9 +1,74 @@
+-- Populando tabela Birds
+-- habitat: dry field, wetlands, dirty field, bush field,
+-- Ardea alba, garça-branca-grande, Great Egret, Família Ardeidae, 65 cm / white
 INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
-            VALUES ('great egret', 'ardea alba', 'garça-real', 'white', 'male', 'wetlands', 'ardeidae', '65 cm');
+            VALUES ('great egret', 'ardea alba', 'garça-real', 'white', 'male', 'wetlands', 'ardeidae', '65');
+-- Cygnus melancoryphus, cisne-de-pescoço-preto, Black-necked-swan, Família Anatidae, 80 / white
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('clack-necked-swan', 'cygnus melancoryphus', 'cisne-de-pescoço-preto', 'white', 'male', '', 'anatidae', '80');
+-- Elanus leucurus, gavião-peneira, White-tailed Kite, Família Accipitridae, 35 / white
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('white-tailed kite', 'elanus leucurus', 'gavião-peneira', 'white', 'female', '', 'accipitridae', '35');
+-- Paroaria coronata, cardeal, Red-crested Cardinal, Família Thraupidae, 17 / white
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('red-crested cardinal', 'paroaria coronata', 'cardeal', 'white', 'male', '', 'thraupidae', '17');
+-- Plegadis chihi, maçarico-preto, White-faced Ibis, Família Threskiornithidae, 40 / darkgray
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('white-faced ibis', 'plegadis chihi', 'maçarico-preto', 'darkgray', 'female', 'wetlands', 'threskiornithidae', '40');
+-- Pseudoleistes virescens, dragão, Brown-and-yellow Marshbird, Família Icteridae, 21 / black
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('brown-and-yellow marshbird', 'pseudoleistes virescens', 'dragão', 'black', 'female', 'bush field', 'icteridae', '21');
+-- Pyrocephalus rubinus, príncipe, Vermilion Flycatcher, Família Tyrannidae, 13 / black
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('vermilion flycatcher', 'pyrocephalus rubinus', 'príncipe', 'black', 'male', 'dry field', 'tyrannidae', '13');
+-- Pygochelidon cyanoleuca, andorinha-pequena-de-casa, Blue-and-white Swallow, Família Hirundinidae, 11 / black
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('southern lapwing', 'vanellus chilensis', 'quero-quero', 'black', 'femele', '', 'hirundinidae', '11');
+-- Vanellus chilensis, quero-quero, Southern Lapwing, Família Charadriidae, 31 / lightgray
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('blue-and-white swallow', 'pygochelidon cyanoleuca', 'quero-quero', 'lightgray', 'femele', 'wetlands', 'charadriidae', '31');
+-- Columbina picui, rolinha-picuí, Picui Ground-Dove, Família Columbidae, 15 / lightgray
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('picui ground-Dove', 'columbina picui', 'rolinha-picuí', 'lightgray', 'male', 'dirty field', 'columbidae', '15');
+-- Polioptila dumicola, balança-rabo-de-máscara, Masked Gnatcatcher, Família Polioptilidae, 12 / gray
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('masked gnatcatcher', 'polioptila dumicola', 'balança-rabo-de-máscara', 'gray', 'male', 'bush field', 'polioptilidae', '12');
+-- Poospiza nigrorufa, quem-te-vestiu, Black-and-rufous Warbling-Finch, Família Thraupidae, 13 / gray
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('black-and-rufous warbling-finch', 'poospiza nigrorufa', 'quem-te-vestiu', 'gray', 'male', '', 'thraupidae', '13');
+-- Amazonetta brasiliensis, marreca-pé-vermelho, Brazilian Teal, Família Anatidae, 35 / brown
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('brazilian teal', 'amazonetta brasiliensis', 'marreca-pé-vermelho', 'brown', 'female', 'wetlands', 'anatidae', '35');
+-- Athene cunicularia, coruja-buraqueira, Burrowing Owl, Família Strigidae, 25 / brown
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('burrowing owl', 'athene cunicularia', 'coruja-buraqueira', 'brown', 'female', 'dry field', 'strigidae', '25');
+-- Chlorostilbon lucidus, besourinho-de-bico-vermelho, Glittering-bellied Emerald, Família Trochilidae, 7  / green
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('glittering-bellied emerald', 'chlorostilbon lucidus', 'besourinho-de-bico-vermelho', 'green', 'mele', 'bush field', 'trochilidae', '7');
+-- Polytmus guaynumbi, beija-flor-de-bico-curvo, White-tailed Goldenthroat, Família Trochilidae, 10 / green
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('white-tailed goldenthroat', 'polytmus guaynumbi', 'beija-flor-de-bico-curvo', 'green', 'female', 'bush field', 'trochilidae', '10');
+-- Pitangus sulphuratus, bem-te-vi, Great Kiskadee, Família Tyrannidae, 23 / yellow
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('great kiskadee', 'pitangus sulphuratus', 'bem-te-vi', 'yellow', 'female', '', 'tyrannidae', '23');
+-- Sicalis flaveola, canário-da-terra-verdadeiro, Saffron Finch, Família Thraupidae, 12 / yellow
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('saffron Finch', 'sicalis flaveola', 'canário-da-terra-verdadeiro', 'yellow', 'female', 'dirty field', 'thraupidae', '12');
+-- Hymenops perspicillatus, viuvinha-de-óculos, Spectacled Tyrant, Família Tyrannidae, 13 / beige
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('Spectacled Tyrant', 'Hymenops perspicillatus', 'viuvinha-de-óculos', 'yellow', 'male', '', 'tyrannidae', '13');
+-- Zonotrichia capensis, tico-tico, Rufous-collared Sparrow, Família Passerellidae, 12 / beije
+INSERT INTO birds(english_name, latin_name, portuguese_name, dominant_color, gender, habitat, family, bird_size) 
+            VALUES ('rufous-collared sparrow', 'zonotrichia capensiss', 'tico-tico', 'yellow', 'female', 'dirty field', 'passerellidae', '12');
 
+
+-- Populando tabela Users
 INSERT INTO users(profile_photo, username, email, password)
             VALUES ('kalpic', 'kal', 'kal@gmail.com', 'kalpassword');
 
+
+
+-- Populando tabela Posts
 INSERT INTO posts(image, location, date ,fk_user, fk_bird)
             VALUES ('grifo', 'condado', '1996', 1, 1);
 INSERT INTO posts(image, location, date ,fk_user, fk_bird)
