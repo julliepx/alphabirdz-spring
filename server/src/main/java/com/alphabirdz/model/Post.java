81459a86f91313d567f8bd2640e865e924cc263a
Jullie Paixão <jlpaixaof@gmail.com>
2022-10-28 05:14:37 -0300

    Adicionado as chaves estrangeiras as tabelas Fix

--- server/src/main/java/com/alphabirdz/model/Post.java
+++ server/src/main/java/com/alphabirdz/model/Post.java
@@ -5,7 +5,6 @@
 
 package com.alphabirdz.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
